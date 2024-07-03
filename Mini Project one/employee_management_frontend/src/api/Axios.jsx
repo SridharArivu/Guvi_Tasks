452080a6079bd6c55f2327a009d82c2a5bfc5c0e
SridharArivu <149089643+SridharArivu@users.noreply.github.com>
2024-07-03 12:12:56 +0530

    Update Axios.jsx

--- Mini Project one/employee_management_frontend/src/api/Axios.jsx
+++ Mini Project one/employee_management_frontend/src/api/Axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-  baseURL: 'https://guvi-mini-project-one-10082893234.development.catalystappsail.com/api/v1',
+   baseURL: 'https://guvi-428306.el.r.appspot.com/api/v1',
   // baseURL: 'http://localhost:9000/api/v1',
 
 });
